b33ec31303edec6ad016fb8fa3cc4d818562d797
nick.harper <nick.harper@capgemini.com>
2022-07-19 10:47:06 +0100

    adding main.tf

---
+++ main.tf
@@ -0,0 +1,74 @@
+provider "aws" {
+  region = "eu-west-2"
+}
+
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_id
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_id
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+  #load_config_file       = false
+  version                = "2.10"
+}
+
+data "aws_availability_zones" "available" {
+}
+
+locals {
+  cluster_name = "prac2-grp1-k8s-test"
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "3.14.2"
+
+  name                 = "k8s-vpc"
+  cidr                 = "10.1.0.0/16"
+  azs                  = data.aws_availability_zones.available.names
+  private_subnets      = ["10.1.1.0/24", "10.1.2.0/24", "10.1.3.0/24"]
+  public_subnets       = ["10.1.4.0/24", "10.1.5.0/24", "10.1.6.0/24"]
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+
+  public_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/elb"                      = "1"
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/internal-elb"             = "1"
+  }
+}
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "18.26.3"
+
+  cluster_name    = "${local.cluster_name}"
+  cluster_version = "1.22"
+  subnet_ids         = module.vpc.private_subnets
+
+  vpc_id = module.vpc.vpc_id
+
+  eks_managed_node_groups = {
+    first = {
+      desired_capacity = 1
+      max_capacity     = 5
+      min_capacity     = 1
+
+      instance_type = "t2.micro"
+    }
+  }
+
+  #write_kubeconfig   = true
+  #config_output_path = "./"
+}
